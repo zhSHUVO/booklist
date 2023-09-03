0c31c5095a6b85ef2019a462c2a205e8a929ca84
zhshuvo <zhshuvo99@gmail.com>
2023-09-03 23:04:03 +0600

    feat : implemented role based route access

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,14 +1,16 @@
 import express from "express";
+import { ENUM_USER_ROLE } from "../../../enums/user";
+import auth from "../../middlewares/auth";
 import { UserController } from "./user.controller";
 
 const router = express.Router();
 
-router.route("/").get(UserController.getAllUsers);
+router.route("/").get(auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
 
 router
     .route("/:id")
-    .get(UserController.getSingleUser)
-    .patch(UserController.updateUser)
-    .delete(UserController.deleteUser);
+    .get(auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser)
+    .patch(auth(ENUM_USER_ROLE.ADMIN), UserController.updateUser)
+    .delete(auth(ENUM_USER_ROLE.ADMIN), UserController.deleteUser);
 
 export const UserRoutes = router;
